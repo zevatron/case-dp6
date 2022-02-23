5c1308fcb62f19070639651d73e2b58b4eca5508
José Ricardo <zevatron@zevatron.com.br>
2022-02-23 14:48:13 -0300

    Incusão eventos: menu, download, analise, contato

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -4,3 +4,57 @@
 // O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
 // para fazer a sua coleta.
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+
+
+function eventoContato() {
+    ga('send', 'event', 'menu', 'entre_em_contato', 'link_externo');
+}
+
+function eventoDownload() {
+    ga('send', 'event', 'menu', 'download_pdf', 'download_pdf');
+}
+
+function eventoAnaliseCard(rotulo) {
+    ga('send', 'event', 'analise', 'ver_mais', rotulo);
+}
+
+function eventoSobre(acao) {
+    if(acao != null){
+        ga('send', 'event', 'contato', acao, 'preencheu');
+    }
+    else {
+        ga('send', 'event', 'contato', 'enviado', 'enviado');
+    }
+}
+
+function load() {
+    var contato = document.getElementsByClassName("menu-lista-contato");
+    contato[0].addEventListener("click",eventoContato,false);
+
+    var download = document.getElementsByClassName("menu-lista-download");
+    download[0].addEventListener("click",eventoDownload,false);
+
+    if(location.pathname == "/analise.html" ) {
+        var cards = document.getElementsByClassName("card card-montadoras");
+        for (const card of cards) {
+            card.addEventListener("click",function(){eventoAnaliseCard(card.dataset.name)},false);       
+        }
+    }
+
+    if(location.pathname == "/sobre.html" ) {
+        var contato = document.getElementsByClassName("contato");
+        inputs = contato[0].getElementsByTagName("input");
+        for (const input of inputs) {
+            input.addEventListener('change',function(){eventoSobre(acao = input.id)},false);
+        }
+    }
+
+    // var mensagemEnviado = jQuery('.lightbox');
+
+
+
+
+
+}
+
+document.addEventListener("DOMContentLoaded", load, false);
